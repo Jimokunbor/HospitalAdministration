7ee210aee5be0cd163181516c9e8cb6eb03fbbb0
Jimokunbor <2022473@student.cct.ie>
2023-11-17 03:32:24 +0000

    Created Patient
    name, birthDate, bloodType, patientID, currentID (Static)
    
    getter for all instance vars
    setter for name

---
+++ src/hospitaladministration/Patient.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitaladministration;
+
+/**
+ *
+ * @author jimok
+ */
+public class Patient {
+    /*
+    Name
+    Date of Birth
+    Blood Type
+    ID
+    */
+    
+    private String name;
+    private String birthDate;
+    private String bloodType;
+    private int patientID;
+
+    public Patient(String name, String birthDate, String bloodType, int patientID) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.bloodType = bloodType;
+        this.patientID = patientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
